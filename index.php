38b4c1b13968fddf6443bc5271557afaa1c8595c
Donald Sebastian Leung <i.donaldl@me.com>
2018-07-12 00:03:59 +0800

    增加圖片

--- index.php
+++ index.php
@@ -7,11 +7,27 @@
 -->
 <html>
 	<head>
-		<title>健一假期旅行社 - 探索澳洲，日本，海南島</title>
+		<title>健一假期旅行社 - 為您提供不一樣的旅遊體驗</title>
 		<meta charset="gb2312" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<script src="assets/libs/skel-3.0.1/dist/skel.min.js"></script>
+		<script src="assets/libs/skel-3.0.1/dist/skel-layout.min.js"></script>
+		<script type="text/javascript">
+			skel.breakpoints({
+				xlarge: "(max-width: 1680px)",
+				large:  "(max-width: 1280px)",
+				medium: "(max-width: 980px)",
+				small:  "(max-width: 736px)",
+				xsmall: "(max-width: 480px)"
+			});
+			skel.layout({
+				reset: "normalize",
+				grid: true,
+				containers: true
+			});
+		</script>
 	</head>
 	<body class="is-preload">
 
@@ -31,7 +47,7 @@
 								<h1>健一假期</h1>
 							</header>
 							<div class="content">
-								<p>和您一起探索澳洲，日本，海南島<br />旅行社代理商牌照號 No: 354016</p>
+								<p>為您提供不一樣的旅遊體驗<br />旅行社代理商牌照號 No: 354016</p>
 							</div>
 						</div>
 					</section>
@@ -103,7 +119,38 @@
 									<header class="major">
 										<h2>關於我們</h2>
 									</header>
-									<p>我們是一間專門提供澳洲，日本和海南島獨立行團服務的旅行社。除此以外，我們也會提供一些郵輪服務和幫助顧客預定機票。</p>
+									<p>我們是一所另類的旅行社，能夠為您度身設計適合您的行程。無論您想去哪裡度假和有多少人同行，我們都可以讓您成團 <span title="必須有二人或以上">*</span>。</p>
+									<div class="row">
+										<div class="6u 12u$(small)">
+											<p><img style="width: 100%" src="images/external/月光園遊月山莊 (2).jpg" alt="月光園遊月山莊 (2)" title="月光園遊月山莊" /></p>
+										</div>
+										<div class="6u 12u$(small)">
+											<p><img style="width: 100%" src="images/external/月光園遊月山莊 (3).jpg" alt="月光園遊月山莊 (3)" title="月光園遊月山莊" /></p>
+										</div>
+									</div>
+									<div class="row">
+										<div class="4u 6u(medium) 12u$(small)">
+											<p><img style="width: 100%" src="images/external/8坂神社 (3).jpg" alt="8坂神社 (3)" title="8坂神社" /></p>
+										</div>
+										<div class="4u 6u(medium) 12u$(small)">
+											<p><img style="width: 100%" src="images/external/有馬溫泉 (3).jpg" alt="有馬溫泉 (3)" title="有馬溫泉" /></p>
+										</div>
+										<div class="4u 12u$(medium)">
+											<p><img style="width: 100%" src="images/external/月光園遊月山莊 (4).jpg" alt="月光園遊月山莊 (4)" title="月光園遊月山莊" /></p>
+										</div>
+									</div>
+									<div class="row">
+										<div class="4u 6u(medium) 12u$(small)">
+											<p><img style="width: 100%" src="images/external/神戶HARBOUR LAND.jpg" alt="神戶HARBOUR LAND" title="神戶HARBOUR LAND" /></p>
+										</div>
+										<div class="4u 6u(medium) 12u$(small)">
+											<p><img style="width: 100%" src="images/external/渡月橋 (2).jpg" alt="渡月橋 (2)" title="渡月橋" /></p>
+										</div>
+										<div class="4u 12u$(medium)">
+											<p><img style="width: 100%" src="images/external/WORLD DREAM.jpg" alt="WORLD DREAM" title="WORLD DREAM" /></p>
+										</div>
+									</div>
+									<p style="font-size: small"><em>以上圖片僅供參考</em></p>
 								</div>
 							</section>
 

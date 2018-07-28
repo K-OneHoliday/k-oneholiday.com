e482d092181503c74feb8d1e7c957028c2292c38
Donald Sebastian Leung <i.donaldl@me.com>
2018-07-28 19:48:49 +0800

    新增旅遊資訊頁面

---
+++ info.php
@@ -0,0 +1,77 @@
+<?php require "assets/php/locale.php"; ?>
+<!DOCTYPE HTML>
+<!--
+	Forty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>旅遊資訊 - 健一假期</title>
+		<meta charset="gb2312" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<?php require "assets/php/header.php"; ?>
+
+				<!-- Menu -->
+					<?php require "assets/php/menu.php"; ?>
+
+				<!-- Main -->
+					<div id="main" class="alt">
+
+						<!-- One -->
+							<section id="one">
+								<div class="inner">
+									<header class="major">
+										<h1><span style="color: cyan" class="icon fa fa-globe"></span> 旅遊資訊</h1>
+									</header>
+									<h3><span style="color: yellow" class="icon fa fa-sun-o fa-spin"></span> 全球天氣預測</h3>
+                  <ul class="alt">
+                    <li><a href="https://www.hko.gov.hk/wxinfo/worldwx/wwi_uc.htm" target="_blank">香港天文台 - 世界天氣資訊</a></li>
+                    <li><a href="http://worldweather.wmo.int/tc/home.html" target="_blank">世界氣象組織 - 世界天氣信息服務網</a></li>
+                    <li><a href="https://www.accuweather.com/zh/world-weather?lang=zh-hk" target="_blank">AccuWeather.com - 世界 天氣</a></li>
+                  </ul>
+                  <h3><span style="color: lime" class="icon fa fa-money"></span> 貨幣兌換</h3>
+                  <ul class="alt">
+                    <li><a href="https://hk.finance.yahoo.com/currency-converter/" target="_blank">雅虎財經 - 貨幣兌換計算機</a></li>
+                    <li><a href="https://www.xe.com/zh-HK/currencyconverter/" target="_blank">捷匯 - 貨幣轉換器</a></li>
+                    <li><a href="http://www.travelex.com.hk/HK/Currency-Converter/" target="_blank">通匯隆 - 貨幣轉換器</a></li>
+                  </ul>
+                  <h3><span style="color: yellow" class="icon fa fa-bolt"></span> 各國電壓</h3>
+                  <ul class="alt">
+                    <li><a href="http://www.tyata.org.tw/common/data/voltage.html" target="_blank">桃園市旅行商業同業公會 - 世界各國電壓表</a></li>
+                    <li><a href="https://www.ifreesite.com/voltage/" target="_blank">線上工具 - 插頭規格</a></li>
+                    <li><a href="http://www.yncnpump.com.tw/D/V.htm" target="_blank">沅晨工業有限公司 - 各國電壓頻率表</a></li>
+                  </ul>
+								</div>
+							</section>
+
+					</div>
+
+				<!-- Contact -->
+					<?php require "assets/php/contact.php"; ?>
+
+				<!-- Footer -->
+					<?php require "assets/php/footer.php"; ?>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>

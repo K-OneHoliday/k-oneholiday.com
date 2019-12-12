873ab742603af3f19d142d9fcde39ca4be2666f9
Donald Sebastian Leung <i.donaldl@me.com>
2019-12-12 14:22:38 +0800

    更改香港總店地址及地圖

--- assets/php/hk_map.php
+++ assets/php/hk_map.php
@@ -1,1 +1,1 @@
-<iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDKQJRY9yChLR8ypQx_tkde-BB3a9T_vYg&amp;q=旺角廣華街48號廣發商業中心&amp;language=zh-tw" allowfullscreen=""></iframe>
+<iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDKQJRY9yChLR8ypQx_tkde-BB3a9T_vYg&amp;q=尖沙咀麼地道63號好時商業中心&amp;language=zh-tw" allowfullscreen=""></iframe>
